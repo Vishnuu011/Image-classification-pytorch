3fe2792ec1acdc22f299c17aecfd813a177ebdb7
Vishnuu011 <vishnurrajeev@gmail.com>
2025-06-04 08:59:24 -0700

    ults update

--- src/_bones/utils/__init__.py
+++ src/_bones/utils/__init__.py
@@ -13,3 +13,44 @@ def read_yaml_file(file_path: str) -> dict:
 
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+def accuracy(outputs, label):
+    try:
+        _, pred=torch.max(outputs, dim=1)
+        return torch.tensor(torch.sum(pred == label).item() / len(pred))
+    except Exception as e:
+        raise CustomException(e, sys)    
+    
+
+def get_default_gpu():
+    try:
+        if torch.cuda.is_available():
+            return torch.device("cuda")
+        else:
+            return torch.device("cpu")
+    except Exception as e:
+        raise CustomException(e, sys)    
+    
+def to_device(data, device):
+    try:
+        if isinstance(data, (list, tuple)):
+            return [to_device(x, device) for x in data]
+        return data.to(device, non_blocking=True)
+    except Exception as e:
+        raise CustomException(e, sys) 
+
+class DeviceDataLoader:
+    def __init__(self, dl, device):
+
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+
+        for d in self.dl:
+            yield to_device(d, self.device)
+
+    def __len__(self):
+        return len(self.dl)    
+    
+                   
